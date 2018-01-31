f2c03c374a3c675859d3bf37a12db09dc1e2cb31
teeweb <tylerweber@live.com>
2018-01-31 12:05:12 -0800

    initialcommit of website wordpress files

---
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
+  	<p>100MILESofMUSIC &copy; 2018 | Site by TWME</p>
+</div><!-- .site-info -->
